649c83c205fbf3f67e9e1d64174b37c18d814359
nikkou0814 <125991634+nikkou-0814@users.noreply.github.com>
2023-07-21 14:41:03 +0900

    asdf

--- bot.py
+++ bot.py
@@ -416,11 +416,12 @@ async def clean_command(interaction: discord.Interaction, how:int):
 @app_commands.checks.has_permissions(moderate_members=True)
 async def ban_command(interaction:discord.Interaction, member: discord.Member, 内容: str, time: str):
   channel = client.get_channel(1123538576525770782)
+  count = update_user_count(username.id)
 
   await interaction.response.defer(ephemeral=True)
 
   embed=discord.Embed(title=f"メンバーがBANされました", color=0x00ff59)
-  embed.set_author(name=f"{(member)}", icon_url=f"{client.user.avatar}")
+  embed.set_author(name=f"<@{username.id}>", icon_url=f"{client.user.avatar}")
   embed.add_field(name="BAN内容",value=f"{(内容)}",inline=False)
   await interaction.followup.send("完了",ephemeral=True)
   embed.set_footer(text=f"version {(VER)} | {(time)} BAN")
